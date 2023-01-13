0ee3c525bbd1549671284ad489fd102d8b518861
saw-code <saw.coding@gmail.com>
2023-01-13 21:11:43 +0300

    1_lesson 3_path

--- src/CurrentBankomat.tsx
+++ src/CurrentBankomat.tsx
@@ -1,14 +1,56 @@
 import React from 'react';
 import {MoneyType} from "./App";
+import styled from "styled-components";
 
 type CurrentBankomatPropsType = {
-    money: MoneyType
+  money: MoneyType
 }
-export const CurrentBankomat = (props:CurrentBankomatPropsType) => {
+export const CurrentBankomat = (props: CurrentBankomatPropsType) => {
 
-    return (
-        <div>
-            {props.money.banknotes}
-        </div>
-    );
+  return (
+    // props.money.banknotes === "Dollars"
+    //   ?
+    //   <BanknoteGreen>
+    //     <Name>{props.money.banknotes}</Name>
+    //     <Nominal>{props.money.value}</Nominal>
+    //   </BanknoteGreen>
+    //   :
+    //   <BanknoteBlue>
+    //     <Name>{props.money.banknotes}</Name>
+    //     <Nominal>{props.money.value}</Nominal>
+    //   </BanknoteBlue>
+
+    <BanknoteUniversal color={props.money.banknotes === "Dollars"
+      ? "greenyellow"
+      : "dodgerblue"}>
+      <Name>{props.money.banknotes}</Name>
+      <Nominal>{props.money.value}</Nominal>
+    </BanknoteUniversal>
+  );
 };
+
+const BanknoteUniversal = styled.div`
+  background: ${props => {
+    if (props.color === "greenyellow") {
+      return "greenyellow"
+    } else {
+      return "dodgerblue"
+    }
+  }};
+  width: 400px;
+  height: 200px;
+  margin: 10px;
+`
+
+const Name = styled.div`
+  display: flex;
+  justify-content: center;
+  font-size: 30px;
+`
+
+const Nominal = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 30px;
+  font-size: 50px;
+`
